2d3219f48eb20a9c5dff5c8012b6c16a5b399e29
neelapeela <boysgood9@gmail.com>
2022-04-13 22:36:31 -0700

    done

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import './App.css';
+import axios from 'axios';
+
+function App() {
+  const user = {
+    name: 'Neel Madhav Dogra',
+    username: 'neelapeela',
+    email: 'psneelmadhav@gmail.com'
+  }
+
+  //read
+  axios.get(`https://jsonplaceholder.typicode.com/users`)
+    .then(request=>{
+      console.log(request.data)
+    })
+  
+  //create
+  axios.post(`https://jsonplaceholder.typicode.com/users`, {user})
+    .then(request=>{
+      console.log(request.data)
+    })
+  
+  //delete
+  const id = 3
+  axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`)
+    .then(request=>{
+      console.log(request.data)
+    })
+
+  //update
+  axios.put(`https://jsonplaceholder.typicode.com/users/1`, {user})
+    .then(request=>{
+      console.log(request.data)
+    })
+
+  return (
+    <div className="App">
+        <h1>Axios Task. (Please open the dev tools console to see axios requests)</h1>
+    </div>
+  );
+}
+
+export default App;
